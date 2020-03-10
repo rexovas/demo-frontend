4cdfe9c2e67fa96d8ee6bc67e445c0c47ccb7ff1
David Lovas <dlovas@rexovas.com>
2020-03-10 16:57:21 -0700

    Release v0.1.2

--- src/App.js
+++ src/App.js
@@ -4,10 +4,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="header">
-        <img src={logo} className="Crossix-logo" alt="logo" />
-      </header>
+    <div className="wrapper">
+      <div className="wrapper__inner">
+        <header className="header">
+          <div className="shell">
+            <div className="header__inner">
+              {/* <a href="/" className="Crossix-Logo"> */}
+              <img src={logo} className="Crossix-logo" alt="Crossix" />
+              {/* </a> */}
+            </div>
+          </div>
+        </header>
+      </div>
     </div>
   );
 }
