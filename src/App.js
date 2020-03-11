8ef7f40b183d2672adc1d3afd14af22aa8a049a2
David Lovas <dlovas@rexovas.com>
2020-03-10 23:18:35 -0700

    Release v0.1.3

--- src/App.js
+++ src/App.js
@@ -9,12 +9,30 @@ function App() {
         <header className="header">
           <div className="shell">
             <div className="header__inner">
-              {/* <a href="/" className="Crossix-Logo"> */}
-              <img src={logo} className="Crossix-logo" alt="Crossix" />
+              {/* <a href="/" className="logo"> */}
+              <img src={logo} className="logo" alt="Crossix" />
               {/* </a> */}
             </div>
           </div>
         </header>
+        <div className="container">
+          {/* <section className="section-slider" data-aos="fade-right">
+            <div className="slider-intro">
+              test
+            </div>
+          </section> */}
+          <section className="section-partners" data-aos="fade-left" id="section-1">
+            <div className="shell">
+              <div className="section__head">
+                <h3>Age-Adjusted Death Rates for the Top 10 Leading Causes of Death in the US</h3>
+              </div>
+              <div className="section__body">
+                test4
+              </div>
+            </div>
+            test2
+          </section>
+        </div>
       </div>
     </div>
   );
