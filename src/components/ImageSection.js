a7a04d3d84614644256b4de38698ba3c0a9f59e6
David Lovas <dlovas@rexovas.com>
2020-03-12 10:31:52 -0400

    complete ImageSection styling

--- src/components/ImageSection.js
+++ src/components/ImageSection.js
@@ -4,7 +4,19 @@ import '../css/components/ImageSection.css';
 const ImageSection = () => (
   <section className="section-slider" data-aos="fade-right">
     <div className="slider-image">
-             TEMPORARY PLACEHOLDER
+      <div className="shell">
+
+        <h1 className="image-header">Interactive Dataset</h1>
+        <p className="image-text">
+          This dataset presents the age-adjusted death rates for the 10 leading causes
+          of death in the United States beginning in 1999. Data are based on information
+          from all resident death certificates filed in the 50 states and the District of
+          Columbia using demographic and medical characteristics
+        </p>
+        <a className="image-link" href="https://catalog.data.gov/dataset/age-adjusted-death-rates-for-the-top-10-leading-causes-of-death-united-states-2013">
+          Source: National Center for Health Statistics
+        </a>
+      </div>
     </div>
   </section>
 );
